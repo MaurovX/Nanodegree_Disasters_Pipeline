76028729ea5af698257f052612eaa3a09067e0ce
MaurovX <54961669+MaurovX@users.noreply.github.com>
2020-12-04 17:25:20 -0500

    Update run.py

--- app/run.py
+++ app/run.py
@@ -40,26 +40,6 @@ from sklearn.base import BaseEstimator,TransformerMixin
 
 app = Flask(__name__)
 
-class StartingVerbExtractor(BaseEstimator, TransformerMixin):
-    def starting_verb(self, text):
-        sentence_list = nltk.sent_tokenize(text)
-        for sentence in sentence_list:
-            pos_tags = nltk.pos_tag(tokenize(sentence))
-            first_word, first_tag = pos_tags[0]
-            if first_tag in ['VB', 'VBP'] or first_word == 'RT':
-                return True
-        return False
-
-    # Given it is a tranformer we can return the self 
-    def fit(self, X, y=None):
-        return self
-
-    def transform(self, X):
-        X_tagged = pd.Series(X).apply(self.starting_verb)
-        return pd.DataFrame(X_tagged)
-  
-    
-    
 def tokenize(text):
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
